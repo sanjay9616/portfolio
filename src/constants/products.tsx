ee833cb1fb7767a743c0e1be99e1bbadd4ecd8de
sanjaymoglix <sanjay.kumar@moglix.com>
2024-07-27 17:29:58 +0530

    --

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -1,11 +1,11 @@
 import portfolioDigimro from "public/images/digimro.png";
 import portfolioDigimro2 from "public/images/digimro2.png";
-import portfolioAlgochurn from "public/images/portfolio-algochurn.png";
-import portfolioAlgochurn2 from "public/images/portfolio-algochurn.png";
-import portfolioMoonbeam from "public/images/portfolio-moonbeam.png";
-import portfolioMoonbeam2 from "public/images/portfolio-moonbeam-2.png";
-import portfolioTailwindMasterKit from "public/images/portfolio-tailwindmasterkit.png";
-import portfolioTailwindMasterKit2 from "public/images/portfolio-tailwindmasterkit-2.png";
+import portfolioEOC from "public/images/eoc.png";
+import portfolioEOC2 from "public/images/eoc-2.png";
+import portfolioCredlix from "public/images/credlix.png";
+import portfolioCredlix2 from "public/images/credlix-2.png";
+import portfolioMaas from "public/images/maas.png";
+import portfolioMaas2 from "public/images/maas-2.png";
 
 export const products = [
   {
@@ -33,91 +33,75 @@ export const products = [
     ),
   },
   {
-    href: "https://algochurn.com",
-    title: "Algochurn",
+    href: "https://eoc.moglix.com/",
+    title: "EOC(Excel on Cloud)",
     description:
-      "Practice for technical interviews with hands on coding challenges.",
-    thumbnail: portfolioAlgochurn,
-    images: [portfolioAlgochurn, portfolioAlgochurn2],
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "algochurn",
+      "Developed an internal application that managed Purchase and Sale orders. I used a custom Excel-like interface to handle items and track the entire lifecycle of orders and products.",
+    thumbnail: portfolioEOC,
+    images: [portfolioEOC, portfolioEOC2],
+    stack: ["Angular", "Angular Material", "RxJS", "HTML", "SCSS"],
+    slug: "eoc",
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+          Developed user interfaces using HTML, CSS, JavaScript, TypeScript,
+          and Angular technologies such as Observables, RxJS, Dependency Injection, Lazy loading, Routing,
+          Directives, Pipes, Reactive forms, Angular Material, and Bootstrap.
+          I also utilized tools like Angular CLI, GitHub for version control, and worked in a Linux environment.{" "}
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
+          I worked on an internal app to create and track Purchase/Sale orders.
+          I designed a custom Excel-like UI to manage order items and built the user interface.
+          I integrated APIs and worked with the design team to create an engaging UI.
+          I also helped connect the visual elements with the server-side infrastructure.
+          Throughout the project, I followed agile methods and attended SCRUM meetings to track progress and issues using JIRA.
         </p>{" "}
       </div>
     ),
   },
   {
-    href: "https://gomoonbeam.com",
-    title: "Moonbeam",
+    href: "https://www.credlix.com/",
+    title: "CREDLIX",
     description:
-      "Never write from scratch again with Moonbeam, your AI first writing tool",
-    thumbnail: portfolioMoonbeam,
-    images: [portfolioMoonbeam, portfolioMoonbeam2],
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "moonbeam",
+      "Credlix is a digital supply chain finance platform from Moglix. We provide quick, collateral-free working capital solutions for enterprises, suppliers, and exporters in India and Southeast Asia to connect to global manufacturing opportunities.",
+    thumbnail: portfolioCredlix,
+    images: [portfolioCredlix, portfolioCredlix2],
+    stack: ["Angular", "Angular Material", "RxJS", "HTML", "SCSS", "CSS", "bootstrap"],
+    slug: "credlix",
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+          At Credlix, we leverage Moglix's expertise in end-to-end supply chain transformation,
+          supporting 700 enterprises and 16,000 SMEs that export to 53 countries worldwide.
+          We combine this with intelligent financial technology to reimagine cash-flow lending.{" "}
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
+          We offer solutions like Early Payment, Vendor Finance, and Channel Finance for enterprises,
+          and Invoice Discounting and Purchase Order Finance for exporters. Our platform connects anchor enterprises,
+          suppliers, exporters, and lenders to keep the supply chain and export finance moving seamlessly..
         </p>{" "}
       </div>
     ),
   },
   {
-    href: "https://tailwindmasterkit.com",
-    title: "Tailwind Master Kit",
+    href: "https://maas.moglix.com/",
+    title: "MAAS",
     description:
-      "A beautiful and comprehensive Tailwind CSS components library for building modern websites and applications.",
-    thumbnail: portfolioTailwindMasterKit,
-    images: [portfolioTailwindMasterKit, portfolioTailwindMasterKit2],
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "tailwindmasterkit",
+      "Developed an Angular app to enhance internal productivity by tracking bulk orders and the order cycle in real-time. Features include Invoice Breakup, Payment Breakup, Credit/Debit Notes, Summary page, sidebar, Customer WPR Remarks, Project Tracker, Customer advances, and PDCs using Angular Material.",
+    thumbnail: portfolioMaas,
+    images: [portfolioMaas, portfolioMaas2],
+    stack: ["Angular", "Angular Material", "RxJS", "HTML", "SCSS"],
+    slug: "maas",
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+          Additionally, the application tracks Customer advances, PDCs, Header,
+          lots, inventory, and stages. The dashboard provides a comprehensive view of order details, financials,
+          and summaries, helping users stay informed and efficient.{" "}
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
+          Using Angular Material, the app delivers a user-friendly interface,
+          making it easier for the team to manage orders and improve overall workflow.
         </p>{" "}
       </div>
     ),
