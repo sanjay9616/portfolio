8fd649ac293d1d4be69ffe6039743fb4871644a5
sanjaymoglix <sanjay.kumar@moglix.com>
2024-07-27 13:47:45 +0530

    --

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -24,9 +24,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     };
   } else {
     return {
-      title: "Projects | John Doe",
+      title: "Projects | Sanjay Kumar",
       description:
-        "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+        "Sanjay Kumar is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
     };
   }
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,9 +7,9 @@ import { Metadata } from "next";
 import Image from "next/image";
 
 export const metadata: Metadata = {
-  title: "Projects | John Doe",
+  title: "Projects | Sanjay Kumar",
   description:
-    "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+    "Sanjay Kumar is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
 };
 
 export default function Projects() {
