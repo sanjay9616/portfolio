b25927323a6e7b7239a5607be55439da87f005c0
sanjaymoglix <sanjay.kumar@moglix.com>
2024-07-29 20:58:10 +0530

    --

--- src/constants/socials.tsx
+++ src/constants/socials.tsx
@@ -13,17 +13,20 @@ export const socials = [
     icon: IconBrandLinkedin,
   },
   {
-    href: "https://github.com/sanjay9616",
+    // href: "https://github.com/sanjay9616",
+    href: "https://www.linkedin.com/in/sanjay9616/",
     label: "Github",
     icon: IconBrandGithub,
   },
   {
-    href: "https://leetcode.com/u/sanjay9616/",
+    // href: "https://leetcode.com/u/sanjay9616/",
+    href: "https://www.linkedin.com/in/sanjay9616/",
     label: "Leetcode",
     icon: IconBrandLeetcode,
   },
   {
-    href: "https://www.codechef.com/users/sanjay9616",
+    // href: "https://www.codechef.com/users/sanjay9616",
+    href: "https://www.linkedin.com/in/sanjay9616/",
     label: "CodeChecf",
     icon: IconCode,
   },
